781ee1ffc3ee68895ec4f6ff5cc093e172002db3
Sanksernebo <karlmarkus.sangernebo@gmail.com>
2023-09-08 09:53:32 +0300

    Week 1 Ex 1

---
+++ src/week1/Ex1.java
@@ -0,0 +1,18 @@
+package week1;
+
+import java.util.Scanner;
+public class Ex1 {
+    public static void main(String[] args) {
+        System.out.println("Karl-Markus Sangernebo");
+
+        String text = "includes text";
+        int wholeNumber = 123;
+        double decimalNumber = 3.141592653;
+        boolean isTrue = true;
+
+        System.out.println("The variable's type is text. Its value is " + text);
+        System.out.println("The variable's type is integer. Its value is  " + wholeNumber);
+        System.out.println("The variable's type is decimal number. Its value is " + decimalNumber);
+        System.out.println("The variable's type is truth value. Its value is " + isTrue);
+    }
+}
