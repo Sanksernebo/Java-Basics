cf90790a17a0b0158aa1b402e39113cf51502bdb
Sanksernebo <karlmarkus.sangernebo@gmail.com>
2023-09-14 14:38:24 +0300

    Week 2 Ex 29 & Ex 37

---
+++ src/week2/Ex29.java
@@ -0,0 +1,13 @@
+package week2;
+
+public class Ex29 {
+    public static void main(String[] args) {
+        // Loop from 2 to 100
+        for (int i = 2; i <= 100; i++) {
+            // Check if the current number is even
+            if (i % 2 == 0) {
+                System.out.println(i); // Print the even number
+            }
+        }
+    }
+}
---
+++ src/week2/Ex37.java
@@ -0,0 +1,17 @@
+package week2;
+
+public class Ex37 {
+        public static void main(String[] args) {
+            // Call the printText method to print the text
+            printText();
+        }
+
+        // Define the printText method
+        public static void printText() {
+            String text = "In the beginning there were the swamp, the hoe and Java.";
+            System.out.println(text); // Print the text
+            System.out.println();    // Print a line break
+        }
+    }
+
+
